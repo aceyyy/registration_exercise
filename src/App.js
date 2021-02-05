ea154f8ab05195dec4439664ed65523c054ef7b8
aceyyyyy <adrianace.aytin@ecashpay.com.ph>
2021-02-05 15:26:54 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  state = {
+    username: "",
+    password: "",
+    confirmPassword: "",
 
-export default App;
+    usernameError: false,
+    passwordError: false,
+    confirmPasswordError: false,
+  }
+
+  onChangeUsername = (e) => {
+    const value = e.target.value;
+    this.setState({ username: value })
+
+    if (value !== undefined && value.length > 0) {
+      if (value.length <= 8) {
+        this.setState({ usernameError: true })
+      } else {
+        this.setState({ usernameError: false })
+      }
+    } else {
+      this.setState({ usernameError: false })
+    }
+  }
+
+  onChangePassword = (e) => {
+    const value = e.target.value;
+    const { confirmPassword } = this.state;
+    this.setState({ password: value })
+
+    if (value !== undefined && value.length > 0) {
+      if (value.length <= 8) {
+        this.setState({ passwordError: true })
+      } else {
+        this.setState({ passwordError: false })
+      }
+
+      if (confirmPassword && value !== confirmPassword) {
+        this.setState({ confirmPasswordError: true })
+      } else {
+        this.setState({ confirmPasswordError: false })
+      }
+
+    } else {
+      this.setState({ passwordError: false })
+    }
+  }
+
+  onChangeConfirmPassword = (e) => {
+    const value = e.target.value;
+    const { password } = this.state;
+    this.setState({ confirmPassword: value })
+
+    if (value !== undefined && value.length > 0) {
+      if (value !== password) {
+        this.setState({ confirmPasswordError: true })
+      } else {
+        this.setState({ confirmPasswordError: false })
+      }
+    } else {
+      this.setState({ confirmPasswordError: false })
+    }
+  }
+
+  render() {
+    const { username, usernameError, password, passwordError, confirmPassword, confirmPasswordError } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Registration Form</h1>
+
+        <div className="div-mb-20 error-msg">
+          {(usernameError || passwordError || confirmPasswordError) && <div>Error/s: </div>}
+          {usernameError && <div>Username must be 8 characters and above</div>}
+          {passwordError && <div>Password must be 8 characters and above</div>}
+          {confirmPasswordError && <div>Confirm Password should be equal to the password</div>}
+        </div>
+
+        <div>
+          <div className="label">Username</div>
+          <input onChange={this.onChangeUsername} />
+        </div>
+        <div>
+          <div>Password</div>
+          <input onChange={this.onChangePassword} />
+        </div>
+        <div>
+          <div>Confirm Password</div>
+          <input onChange={this.onChangeConfirmPassword} />
+        </div>
+        <button
+          type="submit"
+          title="Please input your details first"
+          disabled={(!username || usernameError) || (!password || passwordError) || (!confirmPassword || confirmPasswordError)}
+        >
+          Register
+        </button>
+      </div>
+    )
+  }
+}
